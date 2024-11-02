3906525fceabf02c2d1c8e2ad274871f46274f79
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-02 09:19:12 -0500

    Update variables.tf

--- eks-cluster/variables.tf
+++ eks-cluster/variables.tf
@@ -3,3 +3,95 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "t2s-services-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT gateway"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT gateway"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Enable public access to the EKS cluster endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "enable_cluster_creator_admin_permissions" {
+  description = "Enable admin permissions for the cluster creator"
+  type        = bool
+  default     = true
+}
+
+variable "node_ami_type" {
+  description = "AMI type for EKS managed node groups"
+  type        = string
+  default     = "AL2_x86_64"
+}
+
+variable "eks_managed_node_groups" {
+  description = "Configuration for EKS managed node groups"
+  type        = map(any)
+  default     = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 4
+      desired_size = 4
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+}
